cd0882f0021c7820cea1818d04acdaa9834813dd
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-25 12:47:51 -0300

    configure tailwind.css

---
+++ src/views/Credentials/index.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Aqui é a Credentials</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<h1 class="text-brand-main">Aqui é a Home</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
