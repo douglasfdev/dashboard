d38b04854f31aaddfdd698acc98d06bf836c5c2b
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-26 19:28:33 -0300

    feature: intializing style

---
+++ src/views/Home/Contact.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <main class="flex justify-center w-full">
+    <content class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+      <h1 class="text-4xl font-black text-center text-gray-800">
+        Alguma Dúvida?
+      </h1>
+      <p class="text-lg text-center text-gray-800 font-regular">
+        Quer saber melhor como funciona e quais são os preços?
+      </p>
+      <div class="mt-10">
+        <a href="mailto:douglas.fernandes91@live.com"
+          class="px-6 py-2 mt-10 font-bold text-white rounded-full bg-brand-main focus:outline-none"
+        >
+          Nos mande um e-mail!
+        </a>
+      </div>
+    </content>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,49 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-<h1 class="text-brand-main">Aqui é a Home</h1>
+<custom-header
+  @create-account="handleAccountCreate"
+  @login="handleLogin"
+/>
+<contact />
+
+<footer class="flex justify-center py-10 bg-brand-gray">
+  <p class="font-medium text-center text-gray-800">Feedbacker &#169; 2023</p>
+</footer>
+
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (!token) {
+        router.push({ name: 'Home' })
+      }
+      router.push({ name: 'Feedbacks' })
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
 
