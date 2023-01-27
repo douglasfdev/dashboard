318efc032bb8e76008f4e448f1fd3f9a2d59a2bc
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-26 21:32:21 -0300

    support: comment routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const Home = () => import('../views/Home/index.vue')
+// const Feedbacks = () => import('../views/Feedbacks/index.vue')
+// const Credentials = () => import('../views/Credentials/index.vue')
 
 const routes = [
   {
@@ -8,6 +10,21 @@ const routes = [
     name: 'Home',
     component: Home
   }
+  // ,
+  // {
+  //   path: '/feedbacks',
+  //   name: 'Feedbacks',
+  //   component: Feedbacks
+  // },
+  // {
+  //   path: '/credentials',
+  //   name: 'Credentials',
+  //   component: Credentials
+  // },
+  // {
+  //   path: '/:pathMatch(.*)*',
+  //   redirect: { name: 'Home' }
+  // }
 ]
 
 const router = createRouter({
