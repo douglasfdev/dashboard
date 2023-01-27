80d15d5ee2baf8037955a0a2f80b8a530a287ec3
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-26 21:14:12 -0300

    fix: layout its wrong

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const Home = () => import('../views/Home/index.vue')
-const Feedbacks = () => import('../views/Feedbacks/index.vue')
-const Credentials = () => import('../views/Credentials/index.vue')
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/feedbacks',
-    name: 'Feedbacks',
-    component: Feedbacks,
-    meta: {
-      hasAuth: true
-    }
-  },
-  {
-    path: '/credentials',
-    name: 'Credentials',
-    component: Credentials,
-    meta: {
-      hasAuth: true
-    }
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: { name: 'Home' }
   }
 ]
 
