e7ce82bd906b5b2dc11d79fcf240e975f886c396
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-27 16:10:49 -0300

    feat(modal): Modal Login layout done

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,53 +1,54 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <section class="flex justify-between">
+  <section class="flex justify-between" id="modal-login">
     <h1 class="text-4xl font-black text-gray-800">
       Entre na sua conta
     </h1>
 
     <button
-    @click="close"
+      @click="close"
       class="text-4xl text-gray-600 focus:outline-none"
     >
       &times;
     </button>
   </section>
+
   <section class="mt-16">
     <form @submit.prevent="handleSubmit">
-
       <label class="block">
         <span class="text-lg font-medium text-gray-800">E-mail</span>
         <input
+          id="email-field"
           v-model="state.email.value"
           type="email"
           :class="{
-            'border-brand-danger' : !!state.email.errorMessage
+            'border-brand-danger': !!state.email.errorMessage
           }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="jane.dae@gmail.com"
         >
         <span
-        v-if="!!state.email.errorMessage"
-          class="block font-medium text-brand-danger "
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
         >
           {{ state.email.errorMessage }}
         </span>
       </label>
 
-      <label class="block">
-        <span class="text-lg font-medium text-gray-800">senha</span>
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
         <input
-          v-model="state.senha.value"
+          v-model="state.password.value"
           type="password"
           :class="{
-            'border-brand-danger' : !!state.password.errorMessage
+            'border-brand-danger': !!state.password.errorMessage
           }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="jane.dae@gmail.com"
         >
         <span
-        v-if="!!state.password.errorMessage"
-          class="block font-medium text-brand-danger "
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
         >
           {{ state.password.errorMessage }}
         </span>
@@ -57,7 +58,7 @@
         :disabled="state.isLoading"
         type="submit"
         :class="{
-          'opacity-50' : state.isLoading
+          'opacity-50': state.isLoading
         }"
         class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none
         transition-all duration-150"
