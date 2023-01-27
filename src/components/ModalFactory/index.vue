38635d967fa3c114f9fee9fdb95e5f4b352c057c
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-26 22:17:08 -0300

    feature: modal init

---
+++ src/components/ModalFactory/index.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <teleport to="body">
+    <div
+      class="fixed top-0 left-0 z-50 flex items-center
+      justify-center w-full h-full bg-black bg-opacity-50"
+    >
+      <div class="fixed mx-10">
+
+        <div class="flex flex-col overflow-hidden bg-white
+          rounded-lg animate__animated animate__bounceInDown animate__faster">
+          <div class="flex flex-col px-12 py10 bg-white">
+            <component :is="state.component" />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
