841d5a8b4ece1241d53db67c0c666eea31a1326e
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-27 13:24:41 -0300

    feat(modal): Modal Login initial

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,11 +1,48 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Modal do Login</h1>
+  <section class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">
+      Entre na sua conta
+    </h1>
+
+    <button
+    @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
+      &times;
+    </button>
+  </section>
+  <section class="mt-16">
+    <form>
+
+    </form>
+  </section>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
+import useModal from '@/hooks/useModal'
 export default {
+  setup () {
+    const modal = useModal()
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        error: ''
+      },
+      password: {
+        value: '',
+        error: ''
+      }
+    })
 
+    return {
+      state,
+      close: modal.close
+    }
+  }
 }
 </script>
 
