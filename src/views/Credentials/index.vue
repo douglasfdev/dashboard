9c606f89f2b20c58add47735cbc849082b5e3a83
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-29 12:14:02 -0300

    feature(Credentials): Layout done, needs to checkout users name on button

--- src/views/Credentials/index.vue
+++ src/views/Credentials/index.vue
@@ -1,14 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Aqui é a Credentials</h1>
+  <header class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </header>
+  <section class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Guia de instalação e geração de suas credenciais
+    </p>
+  </section>
 </template>
 
 <script>
+import HeaderLogged from '@/components/HeaderLogged'
+
 export default {
-
+  components: { HeaderLogged }
 }
+
 </script>
-
-<style>
-
-</style>
+}
