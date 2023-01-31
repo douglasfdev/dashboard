eb5d8a74de6a36766566ad8e0429893eb5520023
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-31 00:14:24 -0300

    fix(ModalCreateAccount): placeholder password shows password example

--- src/components/ModalCreateAccount/index.vue
+++ src/components/ModalCreateAccount/index.vue
@@ -68,7 +68,7 @@
             'border-brand-danger': !!state.password.errorMessage
           }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
-          placeholder="jane.dae@gmail.com"
+          placeholder="su4s3nha"
         >
         <span
           v-if="!!state.password.errorMessage"
