b5e1168399127b815006c1d8faf75c2939695d30
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-02-02 21:20:27 -0300

    feat(content-loader): now api contents have a loader

--- src/views/Credentials/index.vue
+++ src/views/Credentials/index.vue
@@ -18,63 +18,125 @@
       <h1 class="text-3xl font-black text-brand-darkgray">
         Instalação e configuração
       </h1>
-     <p class="mt-10 text-lg text-gray-800 font-regular">
+      <p class="mt-10 text-lg text-gray-800 font-regular">
         Este aqui é a sua chave de api
       </p>
 
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
       <div
+        v-else
         class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
-        >
+      >
         <span> {{ store.User.currentUser.apiKey }} </span>
         <div class="flex ml-20 mr-5">
           <icon
             name="copy"
-            :color="brandCollors.graydark"
+            :color="brandColors.graydark"
             size="24"
-            class="cursor-pointer ml-3"
+            class="cursor-pointer"
           />
           <icon
             name="loading"
-            :color="brandCollors.graydark"
+            :color="brandColors.graydark"
             size="24"
             class="cursor-pointer ml-3"
           />
         </div>
       </div>
 
-        <p class="mt-5 text-lg text-gray-800 font-regular">
-          Coloque o script a baixo no seu site para começar a receber feedbacks
-        </p>
+      <p class="mt-5 text-lg text-gray-800 font-regular">
+        Coloque o script abaixo no seu site para começar a receber feedbacks
+      </p>
 
-        <div
-          class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
-          >
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
+      <div
+        v-else
+        class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
+      >
           <pre>
             &lt;script src="https://douglasfdev-feedbacker-widget.vercel.app?api_key={{ store.User.currentUser.apiKey }}"&gt;&lt;/script&gt;"
           </pre>
-        </div>
-
       </div>
+
+    </div>
+
   </section>
 </template>
 
 <script>
-import HeaderLogged from '@/components/HeaderLogged'
-import Icon from '@/components/Icon'
-import useStore from '@/hooks/useStore'
+import { reactive, watch } from 'vue'
+import { useToast } from 'vue-toastification'
+import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
+import Icon from '../../components/Icon'
+import useStore from '../../hooks/useStore'
 import palette from '../../../palette'
+import services from '../../services'
+import { setApiKey } from '../../store/user'
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { ContentLoader, HeaderLogged, Icon },
   setup () {
     const store = useStore()
+    const toast = useToast()
+    const state = reactive({
+      hasError: false,
+      isLoading: false
+    })
+
+    watch(() => store.User.currentUser, () => {
+      if (!store.Global.isLoading && !store.User.currentUser.apiKey) {
+        handleError(true)
+      }
+    })
+
+    function handleError (error) {
+      state.isLoading = false
+      state.hasError = !!error
+    }
+
+    async function handleGenerateApikey () {
+      try {
+        state.isLoading = true
+        const { data } = await services.users.generateApikey()
+
+        setApiKey(data.apiKey)
+        state.isLoading = false
+      } catch (error) {
+        handleError(error)
+      }
+    }
+
+    async function handleCopy () {
+      toast.clear()
+
+      try {
+        await navigator.clipboard.writeText(store.User.currentUser.apiKey)
+        toast.success('Copiado!')
+      } catch (error) {
+        handleError(error)
+      }
+    }
 
     return {
+      state,
       store,
-      brandCollors: palette.brand
+      handleGenerateApikey,
+      handleCopy,
+      brandColors: palette.brand
     }
   }
 }
-
 </script>
-}
